7b586b4c8191ae61a73ca75dca1541ab82f2aa6b
CarlsBad505 <carl@tabrific.com>
2018-12-11 17:56:00 -0700

    first commit

--- db/migrate/20181211184302_create_street_cafes.rb
+++ db/migrate/20181211184302_create_street_cafes.rb
@@ -1,13 +1,12 @@
-class CreateStreetCaves < ActiveRecord::Migration[5.1]
+class CreateStreetCafes < ActiveRecord::Migration[5.1]
   def change
     create_table :street_cafes do |t|
       t.string :name
       t.string :address
       t.string :postal_code
       t.integer :chairs
-      t.string :category
-
-      t.timestamps
+      
+      # t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181211184302) do
+ActiveRecord::Schema.define(version: 20181212005022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 20181211184302) do
     t.string "postal_code"
     t.integer "chairs"
     t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
